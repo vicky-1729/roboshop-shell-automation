9e6d2ab9e20cbf36a31311d708003d78b9cf5cb7
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-21 00:01:02 +0530

    added folder

--- roboshop.sh
+++ roboshop.sh
@@ -3,18 +3,21 @@
 #create access key and secret key in aws
 #make sure aws configure
 
-# Configuration variables
 
-AMI_ID="ami-09c813fb71547fc4f"   # Replace with your AMI ID
-SG_ID="sg-040ecf8bb247d6036"     # Replace with your actual Security Group ID
-ZONE_ID="Z08643193QT2QCZFDKUI1"  # Replace with your actual Route 53 Hosted Zone ID
-DOMAIN_NAME="tcloudguru.in"      # Replace with your domain name
+# Configuration variables
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-040ecf8bb247d6036"
+ZONE_ID="Z08643193QT2QCZFDKUI1"
+DOMAIN_NAME="tcloudguru.in"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 
-aws ec2 run-instances \
-  --image-id ${AMI_ID} \                        # Replace with valid AMI ID
+# Launch the instance
+INSTANCE_ID=$(aws ec2 run-instances \
+  --image-id "$AMI_ID" \
   --instance-type t2.micro \
-  --security-groups ${SG_ID}\         # Replace with your security group
+  --security-group-ids "$SG_ID" \
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyInstance}]' \
-  --count 1
-echo "instance launched successfully ${MyInstance}"
+  --query 'Instances[0].InstanceId' \
+  --output text)
+
+echo "Instance launched successfully: $INSTANCE_ID"
