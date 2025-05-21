e315b70842d8e4b5ceffb78f954079bee5f68acd
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-22 00:26:48 +0530

    added folder

--- catalogue.sh
+++ catalogue.sh
@@ -8,104 +8,98 @@ m="\e[36m"   # Cyan
 s="\e[0m"    # Reset
 
 # Check whether the user is root or not
-if [ $(id -u) -eq 0 ]
-then
-    echo -e "you are a ${m} root user${s} your can directly run the script"
+if [ $(id -u) -eq 0 ]; then
+    echo -e "You are a ${m}root user${s}, you can directly run the script."
 else
-    echo -e "you are ${r} not root user ${s} ,so please run the script using sudo"
+    echo -e "You are ${r}not a root user${s}, please run the script using sudo."
     exit 1
 fi
-
 
 # Log folder setup
 LOG_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
-LOG_FILES="$LOG_FOLDER/$SCRIPT_NAME"
+LOG_FILES="$LOG_FOLDER/$SCRIPT_NAME.log"
 script_dir=$PWD
-#making dirctory
-mkdir -p $LOG_FOLDER
 
+# Making directory
+mkdir -p "$LOG_FOLDER"
+
+# Function to validate each step
 validate() {
-    if [ "$1" -eq 0 ]
-    then
-        echo "$2 is success ...!" | tee -a "$LOG_FILES"
+    if [ "$1" -eq 0 ]; then
+        echo -e "${g}$2 is success ...!${s}" | tee -a "$LOG_FILES"
     else
-        echo "$2 is failure ...!" | tea -a "$LOG_FILES"
+        echo -e "${r}$2 is failure ...!${s}" | tee -a "$LOG_FILES"
     fi
 }
 
-echo "disbaling nodejs...."
+echo -e "${y}Disabling NodeJS module...${s}"
 dnf module disable nodejs -y &>> "$LOG_FILES"
-validate $? "nodjes is disabled"
+validate $? "NodeJS disabling"
 
-
-echo "enabling the nodejs:20...."
+echo -e "${y}Enabling NodeJS:20 module...${s}"
 dnf module enable nodejs:20 -y &>> "$LOG_FILES"
-validate $? "nodejs:20 is enabling"
+validate $? "NodeJS:20 enabling"
 
-echo "installing nodejs....."
+echo -e "${y}Installing NodeJS...${s}"
 dnf install nodejs -y &>> "$LOG_FILES"
-validate $? "nodejs installation"
+validate $? "NodeJS installation"
 
+# Add application user
+id roboshop &>> "$LOG_FILES"
+if [ $? -ne 0 ]; then
+    echo -e "${y}Adding user for roboshop...${s}"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    validate $? "Roboshop user creation"
+else
+    echo -e "${g}User 'roboshop' already exists. Skipping user creation.${s}"
+fi
 
-#Add application User
-echo "adding user for roboshop ..."
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-validate $? "roboshop user creation"
+mkdir -p /app
+validate $? "App folder creation"
 
 rm -rf /app/*
-
-mkdir -p /app 
-validate $? "app folder creation"
-
-
-rm -rf /app/*
-echo "downloading the catalogue zip file ...."
+echo -e "${y}Downloading the catalogue zip file...${s}"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> "$LOG_FILES"
-validate $? "downloading catalogue zip"
+validate $? "Downloading catalogue zip"
 
 cd /app
 
-echo "unzipping the catalogue zip file ...."
+echo -e "${y}Unzipping the catalogue zip file...${s}"
 unzip /tmp/catalogue.zip &>> "$LOG_FILES"
-validate $? "unzipping catalogue zip"
+validate $? "Unzipping catalogue zip"
 
-
-echo "install the dependices..."
+echo -e "${y}Installing dependencies...${s}"
 npm install &>> "$LOG_FILES"
-validate $? "install the dependices"
+validate $? "Dependencies installation"
 
-echo "creating service for catalogue"
-cp $script_dir/services/catalogue.service /etc/systemd/system/catalogue.service  &>> "$LOG_FILES"
-validate $? "service has been created for the catalogue"
+echo -e "${y}Creating service for catalogue...${s}"
+cp "$script_dir/services/catalogue.service" /etc/systemd/system/catalogue.service &>> "$LOG_FILES"
+validate $? "Catalogue service creation"
 
-
+echo -e "${y}Starting the catalogue service...${s}"
 systemctl daemon-reload
-systemctl enable catalogue  
-systemctl start catalogue
-VALIDATE $? "Starting Catalogue"
+systemctl enable catalogue &>> "$LOG_FILES"
+systemctl start catalogue &>> "$LOG_FILES"
+validate $? "Catalogue service start"
 
+echo -e "${y}Creating MongoDB repo file...${s}"
+cp "$script_dir/repos/mongo.repo" /etc/yum.repos.d/mongodb.repo &>> "$LOG_FILES"
+validate $? "MongoDB repo added"
 
-echo "creating the repo for the of mongodb clinet software  "
-cp $script_dir/repos/mongo.repo /etc/yum.repos.d/mongodb.repo &>> "$LOG_FILES"
-validate $? "added mongodb repo"
-
-echo "installling the mongo db..."
+echo -e "${y}Installing MongoDB client...${s}"
 dnf install mongodb-mongosh -y &>> "$LOG_FILES"
-validate $? "installion of mongodb"
+validate $? "MongoDB client installation"
 
-
-STATUS=$(mongosh --host  mongodb.tcloudguru.in --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-if [  "$STATUS" -lt 0 ]
-then
-    echo "loading the data into mongodb ..."
+STATUS=$(mongosh --host mongodb.tcloudguru.in --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ "$STATUS" -lt 0 ]; then
+    echo -e "${y}Loading data into MongoDB...${s}"
     mongosh --host mongodb.tcloudguru.in </app/db/master-data.js &>> "$LOG_FILES"
-    validate $? "loading the data"
+    validate $? "Loading data into MongoDB"
 else
-    echo "echo -e "Data is already loaded ... $Y SKIPPING $s""
+    echo -e "${g}Data is already loaded ... ${y}SKIPPING${s}"
+fi
 
-echo "checking whethaer that ctaalogeu is connecting to mongodb or not ..."
+echo -e "${y}Checking if catalogue can connect to MongoDB...${s}"
 telnet mongodb.tcloudguru.in 27017 &>> "$LOG_FILES"
-validate $? "connection "
-
-
+validate $? "MongoDB connection check"
