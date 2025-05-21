bbb240c725ef57707ed34edb14e9f033e742048c
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-21 22:34:17 +0530

    added folder

--- catalogue.sh
+++ catalogue.sh
@@ -10,9 +10,9 @@ s="\e[0m"    # Reset
 # Check whether the user is root or not
 if [ $(id -u) -eq 0 ]
 then
-    echo -e "you are a $(m)root user$(s) your can directly run the script"
+    echo -e "you are a ${m} root user${s} your can directly run the script"
 else
-    echo -e "you are $(r) not root user $(s) ,so please run the script using sudo"
+    echo -e "you are ${r} not root user ${s} ,so please run the script using sudo"
     exit 1
 fi
 
