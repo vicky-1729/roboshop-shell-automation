e2987b225e61c86efb25b2b0184ea274f3b1fedf
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-24 03:44:36 +0530

    updated content

--- shipping.sh
+++ shipping.sh
@@ -81,16 +81,38 @@ VALIDATE $? "shipping service start "
 dnf install mysql -y &>> "$LOG_FILE"
 VALIDATE $? "installing mysql "
 # Check if the 'cities' database exists and load data if not
-mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 -e "SHOW DATABASES LIKE 'cities';" &>>$LOG_FILE
-if [ $? -ne 0 ]; then
-    mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 -e 'CREATE DATABASE IF NOT EXISTS cities;' &>> "$LOG_FILE"
+# mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 -e "SHOW DATABASES LIKE 'cities';" &>>$LOG_FILE
+# if [ $? -ne 0 ]; then
+#     mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 -e 'CREATE DATABASE IF NOT EXISTS cities;' &>> "$LOG_FILE"
+#     mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 cities < /app/db/schema.sql &>> "$LOG_FILE"
+#     mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 cities < /app/db/app-user.sql &>> "$LOG_FILE"
+#     mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 cities < /app/db/master-data.sql &>> "$LOG_FILE"
+#     VALIDATE $? "Loading database data"
+# else
+#     echo -e "Database data is ${y}already loaded${reset}, skipping..." | tee -a "$LOG_FILE"
+# fi
+
+
+# Check if the 'cities' database exists and load data if not
+# Check if the 'cities' database exists and load data if not
+DB_EXISTS=$(mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 -sse "SHOW DATABASES LIKE 'cities';")
+if [ "$DB_EXISTS" != "cities" ]; then
+    mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 -e 'CREATE DATABASE cities;' &>> "$LOG_FILE"
+    VALIDATE $? "Creating cities database"
+
     mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 cities < /app/db/schema.sql &>> "$LOG_FILE"
+    VALIDATE $? "Loading schema.sql"
+
     mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 cities < /app/db/app-user.sql &>> "$LOG_FILE"
+    VALIDATE $? "Loading app-user.sql"
+
     mysql -h mysql.tcloudguru.in -u root -pRoboShop@1 cities < /app/db/master-data.sql &>> "$LOG_FILE"
-    VALIDATE $? "Loading database data"
+    VALIDATE $? "Loading master-data.sql"
 else
     echo -e "Database data is ${y}already loaded${reset}, skipping..." | tee -a "$LOG_FILE"
 fi
+
+
 
 
 systemctl restart shipping
